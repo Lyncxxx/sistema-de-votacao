b52167d7202aaaf21659df82417ef8ca47686f64
unknown <lynconlima00@gmail.com>
2024-10-03 15:23:14 -0300

    ajustes finais

--- main.py
+++ main.py
@@ -2,6 +2,30 @@ from random import randint
 from time import sleep
 
 
+
+def valida_int(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except:
+            print('ERRO! Informe um número inteiro válido.')
+        else:
+            return n
+
+def valida_cpf(msg):
+    while True:
+        cpf = str(input(msg))
+        if cpf.isnumeric() and len(cpf) == 11:
+            return cpf
+        else:
+            print('ERRO! CPF inválido.')
+
+def titulo(msg):
+    tamanho = len(msg)
+    print('-'*tamanho)
+    print(msg)
+    print('-'*tamanho)
+
 class Candidato:
     def __init__(self, nome, partido, numero):
         self.nome = nome
@@ -45,9 +69,9 @@ class Urna:
     def adicionar_candidato(self, candidato):
         self.lista_de_candidatos.append(candidato)
 
-    def votar(self, eleitor, numero):
+    def votar(self, eleitor):
         if eleitor.pode_votar():
-            # numero = int(input('Informe o número do candidato: '))
+            numero = int(input('Informe o número do candidato: '))
             for candidato in self.lista_de_candidatos:
                 if candidato.numero == numero:
                     # candidato.adicionar_voto()
@@ -59,80 +83,83 @@ class Urna:
 
     def apurar_votos(self):
         urnas_apuradas = 0
+        titulo(f'{urnas_apuradas}% das urnas apuradas')
         for candidato in self.lista_de_candidatos:
-            print('-' * 20)
-            print(f'{urnas_apuradas}% das urnas apuradas')
             print(candidato)
+        sleep(2)
 
         for eleitor in self.lista_de_eleitores:
-            print('-' * 20)
+            urnas_apuradas = randint(urnas_apuradas, 100)
+            titulo(f'{urnas_apuradas}% das urnas apuradas')
             for candidato in self.lista_de_candidatos:
                 if eleitor.n_candidato_votado == candidato.numero:
                     candidato.adicionar_voto()
-                urnas_apuradas = randint(urnas_apuradas,100)
-                print(f'{urnas_apuradas}% das urnas apuradas')
                 print(candidato)
             sleep(2)
-        print('-' * 50)
-
 
     def exibir_vencedor(self):
         vencedor = ''
-        total_de_votos = len(self.lista_de_eleitores)
+        partido_do_vencedor = ''
         num_votos_ganhador = 0
+        total_de_votos = len(self.lista_de_eleitores)
         for candidato in self.lista_de_candidatos:
             print(f'O candidato {candidato.nome} obteve {candidato.votos_recebidos} votos')
             if candidato.votos_recebidos > num_votos_ganhador:
+                partido_do_vencedor = candidato.partido
                 vencedor = candidato.nome
                 num_votos_ganhador = candidato.votos_recebidos
-
         porcentagem_de_votos = round((num_votos_ganhador/total_de_votos)*100, 2)
-        print(f'O vencerdor foi {vencedor} com {porcentagem_de_votos}% dos votos')
+        titulo(f'O vencerdor foi {vencedor} do {partido_do_vencedor}, com {porcentagem_de_votos}% dos votos')
 
 chave_de_encerramento = 1001
-
 urna = Urna()
-# # CADASTRO DOS CANDIDATOS
-# while True:
-#     print('CADASTRO DE CANDIDATO')
-#     nome_candidato = str(input('NOME: '))
-#     partido = str(input('PARTIDO: '))
-#     numero = int(input('NÚMERO: '))
-#     candidato = Candidato(nome_candidato, partido, numero, 'sp')
-#     urna.adicionar_candidato(candidato)
-#     res = input('Deseja adicionar mais algum candidato?[s/n] ').strip().lower()[0]
-#     if res == 'n':
-#         break
-#
-# # PERIODO DE ELEIÇÕES
-# print('ELEIÇÕES 2024')
-# while True:
-#     nome_eleitor = str(input('NOME: '))
-#     if nome_eleitor.isnumeric():
-#         if int(nome_eleitor) == chave_de_encerramento:
-#             break
-#     cpf = int(input('CPF: '))
-#     eleitor = Eleitor(nome_eleitor, cpf)
-#     urna.adicionar_eleitor(eleitor)
-#     urna.votar(eleitor)
-
-candidato1 = Candidato('Candidato 1', 'P1', 1)
-candidato2 = Candidato('Candidato 2', 'P2', 2)
-urna.adicionar_candidato(candidato1)
-urna.adicionar_candidato(candidato2)
 
 
-eleitor1 = Eleitor('Maria', 123)
-urna.adicionar_eleitor(eleitor1)
-urna.votar(eleitor1, 1)
-eleitor2 = Eleitor('Joao', 111)
-urna.adicionar_eleitor(eleitor2)
-urna.votar(eleitor2, 2)
-eleitor3 = Eleitor('Jorge', 1111)
-urna.adicionar_eleitor(eleitor3)
-urna.votar(eleitor3, 1)
+# '''  _            _
+#     | |_ ___  ___| |_
+#     | __/ _ \/ __| __|
+#     | ||  __/\__ \ |_
+#      \__\___||___/\__|'''
+# candidato1 = Candidato('Candidato 1', 'P1', 1)
+# candidato2 = Candidato('Candidato 2', 'P2', 2)
+# urna.adicionar_candidato(candidato1)
+# urna.adicionar_candidato(candidato2)
+# eleitor1 = Eleitor('Maria', 123)
+# urna.adicionar_eleitor(eleitor1)
+# urna.votar(eleitor1, 1)
+# eleitor2 = Eleitor('Joao', 111)
+# urna.adicionar_eleitor(eleitor2)
+# urna.votar(eleitor2, 2)
+# eleitor3 = Eleitor('Jorge', 1111)
+# urna.adicionar_eleitor(eleitor3)
+# urna.votar(eleitor3, 1)
 
+# CADASTRO DOS CANDIDATOS
+while True:
+    print('CADASTRO DE CANDIDATO')
+    nome_candidato = str(input('NOME: '))
+    partido = str(input('PARTIDO: '))
+    while True:
+        numero = valida_int('NÚMERO: ')
+        if 100 > numero > 0:
+            break
+    candidato = Candidato(nome_candidato, partido, numero)
+    urna.adicionar_candidato(candidato)
+    res = input('Deseja adicionar mais algum candidato?[s/n] ').strip().lower()[0]
+    if res == 'n':
+        break
 
+# PERIODO DE ELEIÇÕES
+titulo('ELEIÇÕES 2024')
+while True:
+    nome_eleitor = str(input('NOME: '))
+    if nome_eleitor.isnumeric():
+        if int(nome_eleitor) == chave_de_encerramento:
+            break
+    cpf = valida_cpf('CPF: ')
+    eleitor = Eleitor(nome_eleitor, cpf)
+    urna.adicionar_eleitor(eleitor)
+    urna.votar(eleitor)
 print("\n" * 130)
 urna.apurar_votos()
 urna.exibir_vencedor()
